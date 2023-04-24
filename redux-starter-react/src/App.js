1107078d375118c10d49146d8ff9a501c6620d8b
Tejas Naik <RNTejas2005@gmail.com>
2023-04-24 06:31:12 +0000

    Dispatching Actions

--- redux-starter-react/src/App.js
+++ redux-starter-react/src/App.js
@@ -1,4 +1,15 @@
+import store from "./store";
+
 function App() {
+  console.log(store.getState());
+
+  store.dispatch({
+    type: "bugAdded",
+    payload: {
+      description: "Bug 1"
+    }
+  })
+
   return (
     <div className="App">
       <h1>Redux Starter</h1>
